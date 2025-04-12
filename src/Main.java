f6f7c03b85dd998b6f66639619f2149effd44cf2
Dell <sunilit6624@gmail.com>
2025-04-12 22:19:57 +0530

    second commit deleted

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("git practice");
-        System.out.println("second commit");
         System.out.println("third commit");
         System.out.println("fourth commit");
 
